e969975179b8665315768dcf44aa67c4930ce025
Tim Berggren <tim.berggren@hotmail.com>
2024-11-03 16:41:50 +0100

    Add Book constructor and function to add books to myLibrary array

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const myLibrary = [];
 
 function Book() {
-  // the constructor...
-}
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.info = () => 
+        `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? 'read' : 'not read yet'}`;
+};
 
-function addBookToLibrary() {
-  // do stuff here
-}
+function addBookToLibrary(title, author, pages, hasRead) {
+    const newBook = new Book(title, author, pages, hasRead);
+    myLibrary.push(newBook)
+};
