1fbe1ee2a61e8eb40df4d2654482af881210a939
Tim Berggren <tim.berggren@hotmail.com>
2024-11-03 16:46:13 +0100

    added parameters to constructor function Book

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
-    this.info = () => 
-        `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? 'read' : 'not read yet'}`;
+    this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? 'read' : 'not read yet'}`;
 };
 
 function addBookToLibrary(title, author, pages, hasRead) {
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook)
 };
+
